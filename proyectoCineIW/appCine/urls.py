73918e8994a4ccf169c0bac592e561fe9a6541cb
Aitor Peral <145749077+PeralAitor@users.noreply.github.com>
2024-10-27 16:59:36 +0100

    .

--- proyectoCineIW/appCine/urls.py
+++ proyectoCineIW/appCine/urls.py
@@ -3,10 +3,16 @@ from . import views
 
 urlpatterns = [
     #Url para el listado de provincias, con su correspondiente vista.
-    path('listadoProvincias/', views.listadoProvincias, name='listadoProvincias'),
+    path('listado_provincias/', views.listado_provincias, name='listado_provincias'),
+
+    #Url para el detalle de una provincia.
+    path('detalle_provincia/<int:id_provincia>/', views.detalle_provincia, name='detalle_provincia'),
 
     #Url para el listado de cines, con su correspondiente vista.
-    path('listadoCines/', views.listadoCines, name='listadoCines'),
+    path('listado_cines/', views.listado_cines, name='listado_cines'),
+
+    #Url para el detalle de una provincia.
+    path('detalle_cine/<int:id_cine>/', views.detalle_cine, name='detalle_cine'),
 
     #Url para el listado de sals, con su correspondiente vista.
     path('listadoSalas/', views.listadoSalas, name='listadoSalas'),
--- proyectoCineIW/appCine/views.py
+++ proyectoCineIW/appCine/views.py
@@ -1,23 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound
 from .models import Provincia, Cine, Sala
 # Create your views here.
 
 #Vista de lista de provincias.
-def listadoProvincias(request):
+def listado_provincias(request):
     provincias = Provincia.objects.order_by("id_provincia")
-    texto_provincias = ""
-    for provincia in provincias:
-        texto_provincias += provincia.nombre_provincia + ","
-    return HttpResponse(texto_provincias)
+    contexto = {'lista_provincias': provincias}
+    return render(request, 'lista_provincias.html', contexto)
+
+def detalle_provincia(request, id_provincia):
+    provincia = get_object_or_404(Provincia, id_provincia=id_provincia)
+    cines = provincia.cines.all()  # Obtiene todos los cines relacionados con la provincia
+    context = {'provincia': provincia, 'cines': cines}
+    return render(request, 'detalle_provincia.html', context)
 
 #Vista de lista de cines.
-def listadoCines(request):
+def listado_cines(request):
     cines = Cine.objects.order_by("id_cine")
-    texto_cines = ""
-    for cine in cines:
-        texto_cines += cine.nombre_cine + ","
-    return HttpResponse(texto_cines)
+    contexto = {'lista_cines': cines}
+    return render(request, 'lista_cines.html', contexto)
+
+def detalle_cine(request, id_cine):
+    cines = get_object_or_404(Cine, id_cine=id_cine)
+    contexto = {'cine': cines}
+    return render(request, 'detalle_cine.html', contexto)
 
 #Vista de lista de salas.
 def listadoSalas(request):
