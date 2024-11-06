253c2ea9e61f6cea42e83f75f23a221b4feeab72
isaacherbozo <isaacherbozo06@gmail.com>
2024-11-06 12:38:53 +0100

    Template puesta

--- proyectoCineIW/proyectoCineIW/settings.py
+++ proyectoCineIW/proyectoCineIW/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -99,6 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Internationalization
